83c7d7193e3bd0319c669d29f4306933583dfefe
f-seiko <s2112137@st.tohtech.ac.jp>
2026-03-06 15:56:28 +0900

    初期実装: A1 trial1 の商品ページを作成

---
+++ app/trials/a1/trial1/product/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const onClick = () => {
+  alert("A1のtrial1の商品ページ");
+};
+
+export default function ProductPageA1Trial1() {
+  return (
+    <div>
+      <button onClick={onClick}>！！！！</button>
+    </div>
+  );
+}
