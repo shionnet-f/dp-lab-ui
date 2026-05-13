e174e767af5d521ec294956104d77a3bc5753d58
f-seiko <s2112137@st.tohtech.ac.jp>
2026-05-13 16:14:22 +0900

    fix/trialパスのオーダーをセットごとに指定

--- app/trials/b1/trial2/start/page.tsx
+++ app/trials/b1/trial2/start/page.tsx
@@ -2,10 +2,9 @@ import { TrialStartPage } from "@/app/trials/_components/TrialStartPage";
 import { trial2Data } from "../data";
 import { getTrialPath } from "@/app/trials/_lib/path";
 
-const nextPath = getTrialPath("a1", "trial2", "product")
+const nextPath = getTrialPath("b1", "trial2", "product");
 
 export default function StartPageA1Trial2() {
-
   return (
     <TrialStartPage
       purchaseConditions={trial2Data.purchaseConditions}
